e10c161a7354260b712de0ce31dacae05aa99a6a
rfreitas16 <rfreitas16@hotmail.com>
2024-04-10 12:05:59 -0300

    auto slug produtos

--- produto/admin.py
+++ produto/admin.py
@@ -6,6 +6,7 @@ class VariacaoInline(admin.TabularInline):
     extra = 1
 
 class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta' , 'get_preco_formatado','get_preco_promocional_formatado' ]
     inlines = [VariacaoInline]
 
 admin.site.register(models.Produto, ProdutoAdmin)
